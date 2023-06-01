b9249c2c61ee3b32640220aa0465642f94f28b56
i-prince <molatonny@gmail.com>
2023-06-01 17:41:52 +0300

    Create the first js function
    Prompt the computer to make its choice

--- MyScript.js
+++ MyScript.js
@@ -2,3 +2,10 @@
 
 //Prompting the browser to printout "Hello World"
 console.log ("Hello World");
+
+//Telling the computer to pick its choice that will return either: Rock, Scissor or Paper
+function getComputerChoice (choice){
+    return choice + " is my selection!"
+
+}
+console.log(getComputerChoice ("Rock"))
