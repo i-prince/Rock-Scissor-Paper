97fbf448f98dbdd5f9d1c85174f352a3d737b710
i-prince <molatonny@gmail.com>
2023-07-25 00:29:05 +0300

    Choose the computer's selection randomly
    Furnish the html boilerplate
    Use Math.random to make random selections by the computer

--- MyScript.js
+++ MyScript.js
@@ -1,27 +1,9 @@
-//My external JavaScript
-
-//Prompting the browser to printout "Hello World"
-console.log ("Hello World");
-
-//Telling the computer to pick its choice that will return either: Rock, Scissor or Paper
-function getComputerChoice (choice){
-    return choice + " is my selection!"
-
+// getting the computers random selection
+function getComputerChoice() {
+    const computerChoice = ['Rock', 'Paper', 'Scissor'];
+    const randomSelection = Math.floor(Math.random() * computerChoice.length);
+    return computerChoice[randomSelection];
 }
-console.log(getComputerChoice ("Rock"))
+console.log(getComputerChoice());
 
-//defining the selections
-const playerSelection = "Paper";
-const computerSelection = getComputerChoice
 
-//A function playing a single round of the game
-function firstRound (playerSelection, computerSelection){
-    var lowerCase = playerSelection.toLowerCase() //converting user selection to lowercase
-}
-
-console.log(firstRound (playerSelection, computerSelection));
-
-//looping the firstRound to create the game
-/*function game(firstRound){
-    prompt("what is your choice?", "hello user!")
-}*/
