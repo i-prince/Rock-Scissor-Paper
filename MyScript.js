4ac0b29cbcefa44c7539b6022e80b6b064cc2181
i-prince <molatonny@gmail.com>
2023-09-01 00:32:36 +0300

    Play the game in five rounds
     Use a for loop to loop over the playRound()
     use an if statement to record the scores
     use an if statement to determine the winner

--- MyScript.js
+++ MyScript.js
@@ -4,7 +4,6 @@ function getComputerChoice() {
     const randomSelection = Math.floor(Math.random() * computerChoice.length);
     return computerChoice[randomSelection];
 }
-console.log(getComputerChoice());
 
 // Playing a single round of the game
 function playRound(playerSelection, computerSelection) {
@@ -24,8 +23,33 @@ function playRound(playerSelection, computerSelection) {
         return 'You win! ' + playerSelection + ' beats ' + computerSelection;
     }
 }
-const playerSelection = prompt('what your choice?');
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
+// Function to play a 5 round game
+function game() {  
+    let playerScore = 0; // declare player score and assign its initial value  to zero
+    let computerScore = 0; // declare computer score and assign its initial value to zero
 
+    for (let i = 0; i < 5; i++) { // use the for loop to restrict the conditions between 1-5 instances and to loop over the game
+        const playerSelection = prompt('What is your choice?'); // call the declared playerSelection variable as it was declared and assigned to the value of prompt()
+        const computerSelection = getComputerChoice(); // call the computerSelection variable as it was assigned to the getComputerChoice function
+        const result = playRound(playerSelection, computerSelection); // declare a new variable result and assign it to the playRound function
+        console.log(result); // print out the result
+
+        if (result.includes('win')) { // create an if statement to know whether the player has won the round. Use the JavaScript include function to determine this
+            playerScore++; // add 1 to player score 
+        } else if (result.includes('lose')) { // use the JavaScript include function to know if the player has lost the round
+            computerScore++; // add the 1 to computerScore
+        }
+    }
+
+    if (playerScore > computerScore) {  // use if statement to print out if the player has won or lose the game
+        console.log('Congratulations! You win the game.');
+    } else if (computerScore > playerScore) { // use else if to print out if the player has lost the game
+        console.log('Sorry! You lose the game.');
+    } else { // use else to print a tie
+        console.log("It's a tie game.");
+    }
+}
+
+// Start the game
+game();
